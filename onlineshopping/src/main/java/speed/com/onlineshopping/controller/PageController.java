145a6676316bf3b3a2d07575bd1b9b66879b90b1
pujasashte <S@speed>
2018-05-09 01:50:05 -0700

    sucessfully msg display..

--- onlineshopping/src/main/java/speed/com/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/speed/com/onlineshopping/controller/PageController.java
@@ -2,13 +2,16 @@ package speed.com.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
+	static {
+		System.out.println("Hello");
+	}
 
 	@RequestMapping(value = { "/", "/home", "/index" })
-
 	public ModelAndView index() {
 
 		ModelAndView mv = new ModelAndView("page");
@@ -16,5 +19,13 @@ public class PageController {
 		return mv;
 
 	}
+/*using @RequestParam*/
+	/*@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam("greeting")String greeting) {
 
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+
+	}*/
 }

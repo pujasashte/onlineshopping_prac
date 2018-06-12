5690bc39221f74e6828941e6f0a26122ec815058
pujasashte <S@speed>
2018-06-12 03:54:44 -0700

    changes

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -1,4 +1,3 @@
-
 $(function() {
 	// solving active menu problem
 	switch (menu) {
@@ -31,8 +30,8 @@ $(function() {
 	// execute below code only where we have this table
 	if ($table.length) {
 		// console.log('inside the table');
-		
-// to fetch json data
+
+		// to fetch json data
 		var jsonUrl = '';
 		if (window.categoryId == '') {
 			// document.write("All");
@@ -44,75 +43,85 @@ $(function() {
 					+ window.categoryId + '/products';
 		}
 		// document.write(jsonUrl);
-		$table.DataTable({
-			lengthMenu : [ [ 3, 5, 10, -1 ],
-					[ '3 Records', '5 Records', '10 records', 'ALL' ] ],
-			pageLength : 5,
-			// use of ajax to display json form data
-			ajax: {
-				url: jsonUrl,
-				dataSrc: ''
-			},
-			columns: [ 
-				{
-					data : 'code',
-					bSortable : false,
-					mRender : function(data, type, row) {
+		$table
+				.DataTable({
+					lengthMenu : [ [ 3, 5, 10, -1 ],
+							[ '3 Records', '5 Records', '10 records', 'ALL' ] ],
+					pageLength : 5,
+					// use of ajax to display json form data
+					ajax : {
+						url : jsonUrl,
+						dataSrc : ''
+					},
+					columns : [
+							{
+								data : 'code',
+								bSortable : false,
+								mRender : function(data, type, row) {
 
-						return '<img src="' + window.contextRoot
-								+ '/resources/images/' + data
-								+ '.jpg" class="dataTableImg"/>';
+									return '<img src="' + window.contextRoot
+											+ '/resources/images/' + data
+											+ '.jpg" class="dataTableImg"/>';
 
-					}
-				},	{
+								}
+							},
+							{
 
-				data: 'name'
-			}, {
+								data : 'name'
+							},
+							{
 
-				data: 'brand'
-			}, {
+								data : 'brand'
+							},
+							{
 
-				data: 'unitPrice',
-				mRender:function(data,type,row){
-					return '&#8377; ' + data
-				}
-			},
-			{
-	    		    data: 'quantity',
-	    		    mRender:function(data,type,row){
-	    		     	
-		    	    	if(data < 1){
-		    	    		return '<span style="color:red">Out of Stock!</span>';
-		    	    	}
-		    	    	return data;
-		    	    	}
-	    		    
-		    	   
-	    	},
-	    	{
-	    		data: 'id',
-	    		bSortable: false,
-	    		mRender: function(data,type,row){
-	    			
-	    			var str = '';
-	    			str += '<a href="'+window.contextRoot+ '/show/'+data+'/product" class="btn btn-primary">view product</a> &#160;';
-	    		
-	    			if(row.quantity < 1){
-	    				str += '<a href="javascript:void(0)" class="btn btn-success disabled">add to cart</a>';
-	    	    			
-	    			}
-	    			else{
+								data : 'unitPrice',
+								mRender : function(data, type, row) {
+									return '&#8377; ' + data
+								}
+							},
+							{
+								data : 'quantity',
+								mRender : function(data, type, row) {
 
-		    			str += '<a href="'+window.contextRoot+ '/cart/add'+data+'/product" class="btn btn-success">add to cart</a>';
-		    		     return str;
-	    			}
-	    			
-	    		}
-	    	}
+									if (data < 1) {
+										return '<span style="color:red">Out of Stock!</span>';
+									}
+									return data;
+								}
 
-			]
+							},
+							{
+								data : 'id',
+								bSortable : false,
+								mRender : function(data, type, row) {
 
-		});
+									var str = '';
+									str += '<a href="'
+											+ window.contextRoot
+											+ '/show/'
+											+ data
+											+ '/product" class="btn btn-primary">view product</a> &#160;';
+
+									if (row.quantity < 1) {
+										str += '<a href="javascript:void(0)" class="btn btn-success disabled">add to cart</a>';
+										return str;
+									} else {
+
+										str += '<a href="'
+												+ window.contextRoot
+												+ '/cart/add'
+												+ data
+												+ '/product" class="btn btn-success">add to cart</a>';
+										return str;
+									}
+
+								}
+							}
+
+					]
+
+				});
 	}
 
 });
